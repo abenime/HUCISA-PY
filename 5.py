2b3482deff71bc1f50a2cb9b3fdeed3dce24a907
Abenezer Tilahun <extractiondestruction@gmail.com>
2025-03-19 19:43:07 +0300

    questions 4,5,6

---
+++ 5.py
@@ -0,0 +1,13 @@
+list=[2,4,6,8,10]
+
+#adding a number
+list.append(3)
+print(f'added 3 in the list  --  {list}')
+print()
+#removing from the list
+list.remove(4)
+print(f'removed 4 from the list  --  {list}')
+#max and min
+print()
+print(f'the maximum value is : {max(list)}')
+print(f'the minimum value is : {min(list)}')
---
+++ 6.py
@@ -0,0 +1,12 @@
+students={"student1":"abeni", "age1":25,"student2":"fuad","age2":30}
+
+students["student3"]="megersa"
+students["age3"]=24
+
+print(students)
+
+students["age1"]=15
+
+students.pop("student2")
+students.pop("age2")
+print(students)
